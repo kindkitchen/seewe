fb86e38b56095cbb3467390e934dd27e920cde6e
nik-kita <nik.deno.dev@gmail.com>
2024-08-01 19:00:32 +0200

    url_param_replacer: extend and add tests

--- src/utils/url_param_replacer.util.ts
+++ src/utils/url_param_replacer.util.ts
@@ -1,8 +1,22 @@
-const BETWEEN_CURLY_REGEX = /\{([^}{]+)\}/g
+const BETWEEN_CURLY_REGEX = /\{[^{}]+\}/g
+const AFTER_COLON_REGEX = /:[^\\/]+/g
 
-export const url_param_replacer = (url: string, params: Record<string, string>): string => {
+const between_curly = (url: string, params: Record<string, string>): string => {
   return url.replace(BETWEEN_CURLY_REGEX, (match) => {
     const key = match.slice(1, -1)
     return params[key] || match
   })
+}
+
+const after_colon = (url: string, params: Record<string, string>): string => {
+  return url.replace(AFTER_COLON_REGEX, (match) => {
+    console.log(match)
+    const key = match.slice(1)
+    return params[key] || match
+  })
+}
+
+export const url_param_replacer = {
+  between_curly,
+  after_colon,
 }
