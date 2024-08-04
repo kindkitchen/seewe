ac382d0f75c9da7f3cfb2cd523ddfaf952642daa
nik-kita <nik.deno.dev@gmail.com>
2024-08-04 16:37:09 +0200

    mdcv_service.save_as_default()

--- api/services/md-cv_service.ts
+++ api/services/md-cv_service.ts
@@ -1,5 +1,6 @@
 import { db } from "../db.ts"
 import { MdCv, MdCvEntity } from "../dto/md-cv.dto.ts"
+import { UserEntity } from "../dto/user.dto.ts"
 import { unique_incremental_timestamp } from "../utils/ui/utils/random.util.ts"
 
 const find_by_id = async (
@@ -57,7 +58,71 @@ const update = async (_id: number, mdCv: MdCv) => {
   } as const
 }
 
+const save_as_default = async (mdcv: MdCv, user: UserEntity) => {
+  const _id = mdcv._id || unique_incremental_timestamp()
+  if (user.nik) {
+    void await db._dev_md_cv.updateByPrimaryIndex(
+      "as_default_by_user_id",
+      user._id,
+      {
+        as_default_by_user_id: undefined,
+        as_default_by_username: undefined,
+      },
+    )
+    const default_for_user = {
+      ...mdcv,
+      _id,
+      as_default_by_username: user.nik,
+      as_default_by_user_id: user._id,
+    }
+    const db_res = await db._dev_md_cv.upsertByPrimaryIndex({
+      index: ["_id", mdcv._id],
+      update: default_for_user,
+      set: default_for_user,
+    })
+    if (db_res.ok) {
+      return {
+        ok: true,
+        data: null,
+      } as const
+    } else {
+      return {
+        ok: false,
+        data: null,
+      } as const
+    }
+  } else {
+    const db_res = {
+      ...mdcv,
+      _id,
+      as_default_by_user_id: user._id,
+      as_default_by_username: undefined,
+      as_regulary_by_name_username: undefined,
+      name: undefined,
+      user_id: user._id,
+    }
+    const unnamed_default_result = await db._dev_md_cv.upsertByPrimaryIndex({
+      index: ["as_default_by_user_id", user._id],
+      update: db_res,
+      set: db_res,
+    })
+
+    if (unnamed_default_result.ok) {
+      return {
+        ok: true,
+        data: db_res,
+      } as const
+    } else {
+      return {
+        ok: false,
+        data: null,
+      } as const
+    }
+  }
+}
+
 export const mdCv_service = {
+  save_as_default,
   find_by_id,
   insert,
   update,
