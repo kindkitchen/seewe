55af6bf92c654a58217c3167e16aab84abdd428c
nik-kita <nik.deno.dev@gmail.com>
2024-08-05 08:15:08 +0200

    define base crud mdcv openapi schemas

---
+++ api/routers/mdcv/get_mdcv.ts
@@ -0,0 +1,24 @@
+import { createRoute } from "@hono/zod-openapi"
+import { z } from "zod"
+import { MdCvEntityDto } from "../../dto/md-cv.dto.ts"
+import { authenticated_only_wrapper } from "../../middlewares/authenticated_only.wrapper.ts"
+
+export const delete_mdcv = authenticated_only_wrapper(createRoute({
+  method: "get",
+  path: "/:mdcv_id",
+  request: {
+    params: z.object({
+      mdcv_id: z.string().transform(Number),
+    }),
+  },
+  responses: {
+    200: {
+      description: "CV for owner",
+      content: {
+        "application/json": {
+          schema: MdCvEntityDto,
+        },
+      },
+    },
+  },
+}))
