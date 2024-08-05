8aac4aac729afba912f399811247928ced3261e0
nik-kita <nik.deno.dev@gmail.com>
2024-08-05 13:25:38 +0200

    TagSpeedDial.vue

--- src/register_global_components.ts
+++ src/register_global_components.ts
@@ -2,11 +2,13 @@ import type { App } from "vue"
 import TagButton from "./elements/TagButton.vue"
 import TagH from "./elements/TagH.vue"
 import TagLink from "./elements/TagLink.vue"
+import TagSpeedDial from "./elements/TagSpeedDial.vue"
 
 export const register_global_components = (app: App<Element>): App => {
   app.component("TagButton", TagButton)
   app.component("TagH", TagH)
   app.component("TagLink", TagLink)
+  app.component("TagSpeedDial", TagSpeedDial)
 
   return app
 }
@@ -16,5 +18,6 @@ declare module "vue" {
     TagButton: typeof TagButton
     TagH: typeof TagH
     TagLink: typeof TagLink
+    TagSpeedDial: typeof TagSpeedDial
   }
 }
