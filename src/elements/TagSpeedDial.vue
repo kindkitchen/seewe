8ad7349e4e1ceaa2cb87cf9c33dca92bb297f111
nik-kita <nik.deno.dev@gmail.com>
2024-08-05 17:12:18 +0200

    update tag elements

--- src/elements/TagSpeedDial.vue
+++ src/elements/TagSpeedDial.vue
@@ -8,11 +8,14 @@ const is_open = ref(false)
 </script>
 <template>
   <div :class="tw('inline-flex flex-col sm:flex-row sticky gap-2', attrs.class as any)">
+    <div :class="[{ hidden: !is_open }, 'sm:block']">
+      <slot v-bind="{ is_open }"></slot>
+    </div>
     <tag-button
       @click="() => (is_open = !is_open)"
       :class="
         tw(
-          'text-2xl font-extrabold  h-12 w-12 rounded-full',
+          'text-2xl font-extrabold  h-12 w-12 rounded-full self-end relative bottom-6 -right-3',
           {
             'bg-gray-600 text-gray-300': is_open,
             'text-gray-200 bg-gray-900': !is_open,
@@ -22,8 +25,5 @@ const is_open = ref(false)
       "
       >{{ is_open ? "-" : "+" }}</tag-button
     >
-    <div :class="[{ hidden: !is_open }, 'sm:block']">
-      <slot v-bind="{ is_open }"></slot>
-    </div>
   </div>
 </template>
