afe2c0ef6fefffd7c22b08a07cec9e75de2655a5
nik-kita <nik.deno.dev@gmail.com>
2024-08-05 07:14:12 +0200

    pause

--- api/spa_subserver/spa_subserver.tsx
+++ api/spa_subserver/spa_subserver.tsx
@@ -1,7 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi"
 import { z } from "zod"
 import { db } from "../db.ts"
-import { DotenvFile } from "../env.d.ts"
+import { users_service } from "../services/users_service.ts"
 import { serve_static } from "../utils/serve_static.ts"
 
 export const spa_subserver = new OpenAPIHono()
@@ -20,26 +20,48 @@ export const spa_subserver = new OpenAPIHono()
 
     const db_result = await db._dev_md_cv.findByPrimaryIndex("as_default_by_user_id", user_id)
     const cv = db_result?.value
+
+    // cv not found
     if (!cv) {
+      const db_user_res = await users_service.find_by_id(user_id)
+      const target_user = db_user_res.data
+
+      // check if user has nik and so maybe not default cvs
+      if (target_user) {
+        const { result: public_not_default_cv_list } = await db._dev_md_cv.findBySecondaryIndex(
+          "user_id",
+          user_id,
+          {
+            filter: ({ value }) => value.is_published,
+          },
+        )
+
+        if (public_not_default_cv_list.length > 0) {
+          return ctx.redirect("/" + db_user_res.data.nik)
+        }
+      }
+
+      // nothing to show
       return ctx.notFound()
     }
 
     if (ctx.req.header("authorization")) {
-      // TODO
+      // TODO: if it is owner - add extra functional
     }
 
-    if (cv.name) {
-      return ctx.redirect(Deno.env.get("VITE_API_URL" satisfies keyof DotenvFile)! + "/" + cv.name)
+    // if user already has username this cv should be found by username
+    if (cv.as_default_by_username) {
+      return ctx.redirect("/" + cv.as_default_by_username)
     }
 
     return ctx.html(
-      <html>
+      <>
         <div
           dangerouslySetInnerHTML={{
             __html: cv.html,
           }}
         ></div>
-      </html>,
+      </>,
     )
   })
   .get("/:username/:cv_name", async (ctx) => {
