d0dd33ead508cfdf430381be1e7cc7fb3021a71d
nik-kita <nik.deno.dev@gmail.com>
2024-08-07 16:00:18 +0200

    formkit

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,28 @@ import { createPinia } from "pinia"
 import { DataLoaderPlugin } from "unplugin-vue-router/data-loaders"
 import { router } from "./router/mod.router"
 import { createApp } from "vue"
+import { rootClasses } from "./formkit.theme"
+import { defaultConfig as fkConfig, plugin as fkPlugin } from "@formkit/vue"
 import App from "./App.vue"
 import { bricks } from "./bricks"
 import TH from "./components/TH.vue"
 import TA from "./components/TA.vue"
+import { genesisIcons } from "@formkit/icons"
 
 const app = createApp(App).use(createPinia()).use(DataLoaderPlugin, { router }).use(router)
 
 app.use(...bricks.google_sign_in_plugin())
+app.use(
+  fkPlugin,
+  fkConfig({
+    config: {
+      rootClasses,
+    },
+    icons: {
+      ...genesisIcons,
+    },
+  }),
+)
 
 app.component("t-h", TH)
 app.component("t-a", TA)
--- src/pages/dev.vue
+++ src/pages/dev.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import { FormKit } from "@formkit/vue"
+</script>
+
 <template>
   <t-h>Dev (t-h)</t-h>
+  <hr />
   <t-a to="https://google.com">External link to google (t-a to="https://google.com")</t-a>
   <t-a to="/home">Link to Home page (t-a to="/home")</t-a>
   <t-a :to="{ path: '/home' }">Link to Home page (t-a :to="{ path: '/home' }")</t-a>
   <t-a>Mock link (t-a)</t-a>
+  <hr />
+  <FormKit type="button">Submit</FormKit>
 </template>
