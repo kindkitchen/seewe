8a65478b99ed85fdc44e3e15eaee7103a7e73b7e
nik-kita <nik.deno.dev@gmail.com>
2024-08-07 12:23:42 +0200

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,26 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:storybook/recommended'
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
+    "plugin:storybook/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest",
   },
   rules: {
-    'import/core-modules': ['off', 'vue-router/auto-routes'],
-    'vue/multi-word-component-names': 'off'
-  }
-}
+    "import/core-modules": ["off", "vue-router/auto-routes"],
+    "vue/multi-word-component-names": "off",
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 3,
+      },
+      "multiline": 1,
+    }],
+  },
+};
