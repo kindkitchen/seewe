4a147eaee7b45e7bcee1843940d799f69f8f8220
nik-kita <nik.deno.dev@gmail.com>
2024-08-07 11:50:55 +0200

    rm default stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
-    }
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:storybook/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'import/core-modules': ['off', 'vue-router/auto-routes'],
+    'vue/multi-word-component-names': 'off'
   }
 }
