a9da0bfb6a8e6c973321991206d7c6b0be8deb0f
nik-kita <nik.deno.dev@gmail.com>
2024-08-08 14:16:23 +0200

    update settings

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -3,7 +3,13 @@ import { post_add_nik } from "@/queries/post_add_nik.query"
 import { use_auth } from "@/stores/use_auth.store"
 import type { FormKitNode } from "@formkit/core"
 import { FormKit, setErrors } from "@formkit/vue"
+import Tabs from "primevue/tabs"
+import TabList from "primevue/tablist"
+import Tab from "primevue/tab"
+import TabPanels from "primevue/tabpanels"
+import TabPanel from "primevue/tabpanel"
 
+const auth = use_auth()
 const handle_submit = async ({ nik }: { nik: string }, node: FormKitNode) => {
   try {
     await post_add_nik(nik)
@@ -12,11 +18,24 @@ const handle_submit = async ({ nik }: { nik: string }, node: FormKitNode) => {
     setErrors("settings::add_nik", err)
   }
 }
-const auth = use_auth()
 </script>
 <template>
   <t-h>Settings</t-h>
-  <FormKit id="settings::add_nik" type="form" @submit="handle_submit">
-    <FormKit type="text" name="nik" :value="auth.user?.nik" />
-  </FormKit>
+  <Tabs :value="!auth.user?.nik ? 'username' : undefined">
+    <TabList>
+      <Tab value="username">Username</Tab>
+      <Tab value="cv-visibility-options">CV visibility options</Tab>
+    </TabList>
+    <TabPanels>
+      <TabPanel value="username">
+        <t-h :n="5">Username</t-h>
+        <FormKit id="settings::add_nik" type="form" @submit="handle_submit">
+          <FormKit type="text" name="nik" :value="auth.user?.nik" />
+        </FormKit>
+      </TabPanel>
+      <TabPanel value="cv-visibility-options">
+        <t-h :n="5">Visibility options</t-h>
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
 </template>
