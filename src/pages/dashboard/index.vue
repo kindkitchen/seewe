4a4316dc96a3b6bb1e00edf232ed9c6ba398aeae
nik-kita <nik.deno.dev@gmail.com>
2024-08-13 00:58:44 +0200

    you are logged section

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import { onUnmounted, ref } from "vue"
 import MdCvsList from "./MdCvsList.vue"
+import { use_auth } from "@/stores/use_auth.store";
 
 const loading_dots_count = ref(0)
+const auth = use_auth()
 const loading = setInterval(() => {
   loading_dots_count.value = (loading_dots_count.value + 1) % 16
 }, 50)
@@ -16,8 +18,11 @@ onUnmounted(stop_loading)
   </div>
   <Suspense :on-resolve="stop_loading">
     <template #fallback>
-      <t-h> {{ "loading" + ".".repeat(loading_dots_count) }}</t-h></template
-    >
+      <t-h> {{ "loading" + ".".repeat(loading_dots_count) }}</t-h></template>
     <MdCvsList />
   </Suspense>
+  <div class="text-emerald-700 italic">
+    <p>You are logged as {{ auth.user?.name }}</p>
+    <p>{{ auth.user?.email }}</p>
+  </div>
 </template>
