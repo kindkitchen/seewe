85b60629d87c4018077b2e890d66150be807a2c1
nik-kita <nik.deno.dev@gmail.com>
2024-08-12 19:15:56 +0200

    fix deletion

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup>
+import { onUnmounted, ref } from "vue"
 import MdCvsList from "./MdCvsList.vue"
+
+const loading_dots_count = ref(0)
+const loading = setInterval(() => {
+  loading_dots_count.value = (loading_dots_count.value + 1) % 7
+}, 200)
+const stop_loading = () => clearInterval(loading)
+onUnmounted(stop_loading)
 </script>
 
 <template>
@@ -7,7 +15,7 @@ import MdCvsList from "./MdCvsList.vue"
     <h1>Dashboard</h1>
     <p>Welcome to your dashboard</p>
   </div>
-  <Suspense>
+  <Suspense :on-resolve="stop_loading">
     <template #fallback>loading...</template>
     <MdCvsList />
   </Suspense>
