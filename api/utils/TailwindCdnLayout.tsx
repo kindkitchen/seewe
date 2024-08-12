5576b29c94ce291b7bf292cec816a78392380a83
nik-kita <nik.deno.dev@gmail.com>
2024-08-12 19:53:43 +0200

    commit

--- api/utils/TailwindCdnLayout.tsx
+++ api/utils/TailwindCdnLayout.tsx
@@ -4,23 +4,6 @@ export const TailwindCdnLayout: FC = (props) => {
   return (
     <html>
       <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossorigin="anonymous"
-        ></link>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
-          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-          crossorigin="anonymous"
-        ></link>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
-          integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-          crossorigin="anonymous"
-        ></script>
         <style>
           {`\
 p, ul, ol, li, h3, h4, h5, h6 {
