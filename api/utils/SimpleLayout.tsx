3f052ef24a9b9a495bc8248a3a823fafabecace7
nik-kita <nik@kita.github>
2025-06-25 00:12:01 +0200

    update deploy.yml

--- api/utils/SimpleLayout.tsx
+++ api/utils/SimpleLayout.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "hono/jsx"
 import { html } from "hono/html"
 
-const css = String.raw;
+const css = String.raw
 export const SimpleLayout: FC = (props) => {
   const print_pdf = props.print_pfd ?? true
   return (
@@ -9,55 +9,52 @@ export const SimpleLayout: FC = (props) => {
       <head>
         <link rel="stylesheet" href="https://newcss.net/theme/night.css"></link>
         <style>
-          {
-            css`
-                p, ul, ol, li, h3, h4, h5, h6 {
-                  break-inside: avoid-page;
-                  break-before: avoid-page;
-                }
+          {css`
+            p, ul, ol, li, h3, h4, h5, h6 {
+              break-inside: avoid-page;
+              break-before: avoid-page;
+            }
 
-                p, ul, ol, li, h1, h2, h3, h4, h5, h6 {
-                  break-after: avoid-page;
-                }
+            p, ul, ol, li, h1, h2, h3, h4, h5, h6 {
+              break-after: avoid-page;
+            }
 
-                h1 {
-                  text-align: center;
-                }
+            h1 {
+              text-align: center;
+            }
 
-                h1, h2 {
-                  text-wrap: pretty;
-                  break-before: auto;
-                }
+            h1, h2 {
+              text-wrap: pretty;
+              break-before: auto;
+            }
 
-                hr {
-                  border: none;
-                  border-top: 1px dotted;
-                  break-after: auto;
-                  background-color: green;
-                }
+            hr {
+              border: none;
+              border-top: 1px dotted;
+              break-after: auto;
+              background-color: green;
+            }
 
-                body {
-                  color: black;
-                  padding-inline: 2rem;
-                  padding-top: 2rem;
-                }
-                .water-mark:hover {
-                  color: green;
-                }
-                .water-mark {
-                  color: lightgray;
-                  position: fixed;
-                  top: 0;
-                  right: 0;
-                }
+            body {
+              color: black;
+              padding-inline: 2rem;
+              padding-top: 2rem;
+            }
+            .water-mark:hover {
+              color: green;
+            }
+            .water-mark {
+              color: lightgray;
+              position: fixed;
+              top: 0;
+              right: 0;
+            }
 
-                @media print
-                {    
-                    .no-print, .no-print *
-                    {
-                        display: none !important;
-                    }
-                }
+            @media print {
+              .no-print, .no-print * {
+                display: none !important;
+              }
+            }
           `}
         </style>
       </head>
