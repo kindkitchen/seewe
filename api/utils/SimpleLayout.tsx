1df10c1c75b1fe55ac25d6fd40742992f1255247
nik-kita <nik@kita.github>
2025-06-28 01:00:49 +0200

    css

--- api/utils/SimpleLayout.tsx
+++ api/utils/SimpleLayout.tsx
@@ -41,7 +41,7 @@ export const SimpleLayout: FC = (props) => {
             }
 
             .water-mark {
-              font-size: 06em;
+              font-size: 0.6em;
               color: lightgray;
               position: fixed;
               top: 0;
