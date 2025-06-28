be421008c1a7c2133755adad6b01262ff621aa39
nik-kita <nik@kita.github>
2025-06-28 03:48:14 +0200

    fix, refactor

--- api/spa_subserver/spa_subserver.tsx
+++ api/spa_subserver/spa_subserver.tsx
@@ -61,9 +61,9 @@ export const spa_subserver = new OpenAPIHono()
     )
 
     if (happy_path_result?.value) {
-      const link = config.VITE_API_URL + "/" + username + "/" + cv_name;
+      const link = config.VITE_API_URL + "/" + username + "/" + cv_name
 
-      console.log(happy_path_result.value.html);
+      console.log(happy_path_result.value.html)
 
       return ctx.html(
         <SimpleLayout link={link}>
@@ -71,8 +71,7 @@ export const spa_subserver = new OpenAPIHono()
             dangerouslySetInnerHTML={{
               __html: happy_path_result.value.html,
             }}
-          >
-          </div>
+          />
         </SimpleLayout>,
       )
     }
